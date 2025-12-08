2bed3b93327b50bb318ba5337028f1786691ed00
Kevin Flynn <kevinflynn@kevins-mbp.myfiosgateway.com>
2025-12-08 18:36:06 -0500

    fork gtfs project, fix unknown station error, add q train data, remove 1 train data

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import flask
 import datetime
 import pytz
-from nyct_gtfs import NYCTFeed
+import importlib
+from nyct_gtfs.feed import NYCTFeed
 
 app = flask.Flask(__name__)
 
@@ -15,20 +16,26 @@ def hello_world():
 def get_trains():
     output = {
         'debug': len(NYCTFeed("2").trips),
-        'N': {
+        'N-23': {
             'trips': [],
             'trains_stopping_at_grand_army': False,
             'trains_stopping_at_wall_street': False,
             'trains_stopping_at_penn_station': False,
             'trains_stopping_at_lincoln_center': False
         },
-        'S': {
+        'S-23': {
             'trips': [],
             'trains_stopping_at_grand_army': False,
             'trains_stopping_at_penn_station': False,
             'trains_stopping_at_wall_street': False,
             'trains_stopping_at_lincoln_center': False
         },
+        'N-Q': {
+            'trips': [],
+        },
+        'S-Q': {
+            'trips': [],
+        }
     }
 
     feed_1_2_3 = NYCTFeed("2")
@@ -39,7 +46,7 @@ def get_trains():
         input = {
             "summary": str(trip)
         }
-        if trip.route_id not in ['4', '5', '6', '7', 'GS']:
+        if trip.route_id not in ['1', '4', '5', '6', '7', 'GS']:
             input['route_id'] = trip.route_id
             stop_time_updates = trip.stop_time_updates
             for update in stop_time_updates:
@@ -53,19 +60,41 @@ def get_trains():
                     arrival_est = "-"
                 if update.stop_name == 'Grand Army Plaza':
                     input['grand_army_arrival'] = arrival_est
-                    output[trip.direction]['trains_stopping_at_grand_army'] = True
+                    output[f'{trip.direction}-23']['trains_stopping_at_grand_army'] = True
                 elif update.stop_name == 'Wall St':
                     input['wall_street_arrival'] = arrival_est
-                    output[trip.direction]['trains_stopping_at_wall_street'] = True
+                    output[f'{trip.direction}-23']['trains_stopping_at_wall_street'] = True
                 elif update.stop_name == '34 St-Penn Station':
                     input['penn_station_arrival'] = arrival_est
-                    output[trip.direction]['trains_stopping_at_penn_station'] = True
-                elif update.stop_name == '66 St-Lincoln Center':
-                    input['lincoln_center_arrival'] = arrival_est
-                    output[trip.direction]['trains_stopping_at_lincoln_center'] = True
+                    output[f'{trip.direction}-23']['trains_stopping_at_penn_station'] = True
 
-            output[trip.direction]['trips'].append(input)
-    
+            output[f'{trip.direction}-23']['trips'].append(input)
+    feed_q = NYCTFeed("Q")
+    sorted_trips = sorted(feed_q.trips, key=lambda trip: trip.departure_time)
+    for trip in sorted_trips:
+        input = {
+            "summary": str(trip)
+        }
+        if trip.route_id == 'Q':
+            input['route_id'] = trip.route_id
+            stop_time_updates = trip.stop_time_updates
+            for update in stop_time_updates:
+                if update.arrival is not None:
+                    est = pytz.timezone('US/Eastern')
+                    arrival_est = datetime.datetime(arrival_gmt.year, arrival_gmt.month, arrival_gmt.day, arrival_gmt.hour, arrival_gmt.minute, arrival_gmt.second, tzinfo=est)
+                    arrival_est = arrival_est - datetime.timedelta(hours=5)
+                    arrival_est = arrival_est.strftime('%H:%M')
+                else:
+                    arrival_est = "-"
+                if update.stop_name == '7 Av':
+                    input['seventh_av_arrival'] = arrival_est
+                elif update.stop_name == '14 St-Union Sq':
+                    input['union_square_arrival'] = arrival_est
+                elif update.stop_name == 'Times Sq-42 St':
+                    input['times_square_arrival'] = arrival_est 
+
+            output[f'{trip.direction}-Q']['trips'].append(input)
+
     response = flask.jsonify(output)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
