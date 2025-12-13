d8f165fa25bdf0623e8f00153de3744fe2309123
Kevin Flynn <kevinflynn@kevins-mbp.myfiosgateway.com>
2025-12-13 00:58:51 -0500

    add response code to refresh endpoints

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ app = flask.Flask(__name__)
 # app.config['REDIS_URL'] = "redis://localhost:6379/0"
 
 # EXTERNAL URL FOR TESTING LOCALLY
-# app.config['REDIS_URL'] = 'rediss://red-d4udg92li9vc73d3dq5g:UV1ExfzjfZPgLJeE1J307BlwhdC3HyUz@virginia-keyvalue.render.com:6379'
+app.config['REDIS_URL'] = 'rediss://red-d4udg92li9vc73d3dq5g:UV1ExfzjfZPgLJeE1J307BlwhdC3HyUz@virginia-keyvalue.render.com:6379'
 
 # INTERNAL URL FOR DEPLOYMENT
-app.config['REDIS_URL'] = 'redis://red-d4udg92li9vc73d3dq5g:6379'
+# app.config['REDIS_URL'] = 'redis://red-d4udg92li9vc73d3dq5g:6379'
 redis_client = redis.from_url(app.config['REDIS_URL'])
 
 # TODO: add daily / half hourly cache refresh cron jobs on screenings and news
@@ -102,6 +102,8 @@ def get_cached_result(key, refresh_only=False):
         response = flask.jsonify(response)
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
+    else:
+        return 'Refresh OK', 200
 
 @app.route('/kpi/trains')
 def get_trains_from_cache_or_live():
